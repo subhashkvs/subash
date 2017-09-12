a1f352dba022d4ae1a6f0568a67cd9f809271b82
subhashkvs <30828609+subhashkvs@users.noreply.github.com>
2017-09-12 15:47:56 +0530

    Create pos.c

---
+++ pos.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n;
+clrscr();
+printf("enter the number");
+scanf("%d",&n);
+if(n>o)
+}
+printf("ihe given number is postive");
+}
+else
+{
+printf("the given number is negative");
+}
+getch();
+}
