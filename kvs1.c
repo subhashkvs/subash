106eb61d9b85b5278b3651742dcb88cb846a4df4
subhashkvs <30828609+subhashkvs@users.noreply.github.com>
2017-09-12 16:33:27 +0530

    Create kvs1.c

---
+++ kvs1.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char c;
+char a[5]={a,e,i,o,u}
+printf("enter the character");
+scanf("%c",&c);
+for(i=0;i<5;i++)
+if(a[i]==c)
+{
+printf("given letter is vowel");
+}
+else
+{
+printf("not vowel");
+}
+}
+getch();
+}
