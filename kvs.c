0088f3ae53de22bf5d63b71775fde579cb051b06
subhashkvs <30828609+subhashkvs@users.noreply.github.com>
2017-09-12 16:20:50 +0530

    Create kvs.c

---
+++ kvs.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n;
+clrscr();
+scanf("%d",&n);
+if(n%2==0)
+{
+printf("given number is even");
+}
+else
+{
+printf("given number is odd");
+}
+getch();
+}
