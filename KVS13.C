f3bb77578fec3d7c911f9f6266f19f7b47d96588
subhashkvs <30828609+subhashkvs@users.noreply.github.com>
2018-01-11 16:56:08 +0530

    Create KVS13.C

---
+++ KVS13.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int n;
+printf("enter the number");
+scanf("%d",&n);
+if(n/n!=0&&n/1!=n)
+{
+printf("given number is prime");
+}
+else
+{
+printf("not prime");
+}
+}
