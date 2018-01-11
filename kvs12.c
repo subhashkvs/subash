73bff9f140b7a0a417ab11a5e6ccec9abc89ff32
subhashkvs <30828609+subhashkvs@users.noreply.github.com>
2018-01-11 16:15:49 +0530

    Create kvs12.c

---
+++ kvs12.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,r=0,temp,b;
+printf("enter the number");
+scanf("%d",&n);
+int a=n
+while(n!=0)
+{
+temp=n%10;
+r=r*10+temp;
+n=n/10;
+}
+if(a=r)
+{
+printf("the given string is palindrom");
+}
+ele
+{
+printf("the given string is not palindron);
+}
+getch();
+}
