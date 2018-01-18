2785234af6f663eb4aba464faab1c8bd912c1eba
subhashkvs <30828609+subhashkvs@users.noreply.github.com>
2018-01-18 16:30:17 +0530

    Create kvs11.c

---
+++ kvs11.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int b,p;
+printf("enter the base and power value");
+scanf("%d%d",&b,&p);
+c=b^p;
+printf("the value ofc is %d",&c);
+getch();
+}
