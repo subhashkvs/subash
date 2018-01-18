c286ca58fcd235bd53d954f6da28c2ba55f1e668
subhashkvs <30828609+subhashkvs@users.noreply.github.com>
2018-01-18 16:11:01 +0530

    Create krs9.c

---
+++ krs9.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n[i]={1,2,3,4,5};
+int k,i,c=0;
+printf("enter the value of k");
+scanf("%d",&k);
+for(i=0;i<k;i++)
+{
+c=n[i]+c;
+}
+printf("the sum is=%d",&c);
+getch();
+}
