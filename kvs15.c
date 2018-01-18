0f2c7d4d06842711a772ced436ae27601322351c
subhashkvs <30828609+subhashkvs@users.noreply.github.com>
2018-01-18 16:52:33 +0530

    Create kvs15.c

---
+++ kvs15.c
@@ -0,0 +1,18 @@
+#include<stdi.h>
+#include<conio.h>
+void main()
+{
+int i,a,n;
+printf("enter the value of a");
+scanf("%d",&a);
+printf("enter the value of n");
+scanf("%d",&n);
+for(i=a+1;i<n;i++)
+{
+if(i%2==0)
+{
+printf("the value is %d",&i);
+}
+}
+getch();
+}
