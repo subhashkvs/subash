50e426925cf0cd7c47f57ba3e56a73f440572760
subhashkvs <30828609+subhashkvs@users.noreply.github.com>
2018-01-21 09:37:05 +0530

    Create kvs22.c

---
+++ kvs22.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,c;
+printf("enter the three number");
+scanf("%d%d%d",&a,&b,&c);
+if((a>b)&&(a>c))
+{
+printf("%d",&a);
+}
+else if(b>c)
+{
+printf("%d",&b)
+}
+else
+{
+printf("%d",&c);
+}
+getch();
+}
