2b05ca289fa37de89b59c49e8609fb09b5ac2f0d
subhashkvs <30828609+subhashkvs@users.noreply.github.com>
2018-01-21 11:15:59 +0530

    Update kvs25.c

--- kvs25.c
+++ kvs25.c
@@ -3,10 +3,10 @@
 int main()
 {
 int a[10]={3,2,6,5,4,1,8,7,9};
-int i,j,temp;
- for(i=0;i<a[10];i++)
+int i,j,temp,b,c;
+ for(i=0;i<10;i++)
  {
- for(j=i+1;j<a[10];j++)
+ for(j=i+1;j<10;j++)
  {
  if(a[i]<a[j])
  {
@@ -14,10 +14,11 @@ int i,j,temp;
    a[i]=a[j];
    a[j]=temp;
  }
-  for(i=0;i<a[10];i++)
+  for(i=0;i<10;i++)
   {
     printf("%d",a[i]);
   }
- b=a[10]/2;
- printf("%d",b);
+ b=10/2;
+  c=a[b];
+  printf("%d",c);
  }
