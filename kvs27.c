a0a522518434a54d65f022a5aaa8e87b963a538a
subhashkvs <30828609+subhashkvs@users.noreply.github.com>
2018-01-21 11:35:09 +0530

    Create kvs27.c

---
+++ kvs27.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int c;
+printf("enter the string");
+scanf(%d",&c);
+if((c>='0')&&(ch<='9'))
+{
+printf("it is numeric");
+}
+else
+{
+printf("not numeric");
+}
+getch();
+}
