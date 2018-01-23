b4eff9e5dc948c0fbfc40eae4ca9f54ee38708f0
subhashkvs <30828609+subhashkvs@users.noreply.github.com>
2018-01-23 16:23:44 +0530

    Update kvs39.c

--- kvs39.c
+++ kvs39.c
@@ -3,7 +3,22 @@
 void main()
 {
 int a[10]={5,4,3,2,1,6,10,8,9};
-int temp,i;
-for(i=0;i<10;i++)
-{
+int i,j,temp;
+ for(i=0;i<10;i++)
+ {
+ for(j=i+1;j<10];j++)
+ {
+ if(a[i]<a[j])
+ {
+  temp=a[i];
+   a[i]=a[j];
+   a[j]=temp;
+ }
+  for(i=0;i<10;i++)
+  {
+    printf("%d",a[i]);
+  }
+   printf("the maximum number is :%d",a[10]);
+   getch();
+}
 
