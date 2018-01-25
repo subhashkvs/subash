8f639e247a170b803f371e0e7d646d148a84420f
subhashkvs <30828609+subhashkvs@users.noreply.github.com>
2018-01-25 16:04:16 +0530

    Create kvs31.c

---
+++ kvs31.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int a[100];
+int i,n,count;
+pritf("enter th sentence");
+scanf("%d",&a);
+for(i=0;i<n;i++)
+{
+if(a[i]!=" ")
+{
+count++;
+}
+}
+printf("%d",count);
+}
