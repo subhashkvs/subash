5c26ee95ddd002dd056cdde18598136dd0bf45a6
subhashkvs <30828609+subhashkvs@users.noreply.github.com>
2018-01-25 16:53:31 +0530

    Create krs35.c

---
+++ krs35.c
@@ -0,0 +1,21 @@
+#includkrs35.ce<stdio.h>
+#include<conio.h>
+#include<string.h>
+int main()
+{
+char*a[100];
+int i,n,b,count;
+printf("enter the sentence");
+for(i=0;i<n;i++)
+{
+scanf("%s",&a[i]);
+}
+n=strlen(a);
+for(i=0;i<n;i++)
+{
+if(a[i]=="0-9")
+{
+count++;
+}
+getch();
+}
