880d12a548f858227c7cb639c8bc186d4bfa7126
subhashkvs <30828609+subhashkvs@users.noreply.github.com>
2018-01-25 16:42:28 +0530

    Create krs32.c

---
+++ krs32.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+char*a[100];
+int i,n,b,count;
+printf("enter the sentence");
+for(i=0;i<n;i++)
+{
+scanf("%s",&a[i]);
+}
+n=strlen(a);
+for(i=0;i<n;i++)
+{
+if(a[i]=="\0")
+{
+count++;
+}
+}
