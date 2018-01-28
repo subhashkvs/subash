d3ad07d67b943d79cd74eb37d671ac2e04261540
subhashkvs <30828609+subhashkvs@users.noreply.github.com>
2018-01-28 06:12:15 +0000

    Create kvs38.c

---
+++ kvs38.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,c,n;
+printf("enter the two number");
+scanf("%d",&a);
+n=a%10;
+c=a/10;
+printf("swaped number is %d%d",n,c);
+getch();
+}
