b80a04054c4b50c8963886e8d3dbec9a79ff2bcb
subhashkvs <30828609+subhashkvs@users.noreply.github.com>
2018-02-01 16:41:22 +0530

    Create kkk3.c

---
+++ kkk3.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+void main()
+{
+char a[100],b[100];
+printf("enter the two line:");
+scanf("%s %s",&a,&b);
+printf("\n%s%s",a,b);
+getch();
+}
