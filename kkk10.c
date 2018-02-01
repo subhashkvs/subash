c3ce7255131f1cabe3af4604d949b9c1e2cfb137
subhashkvs <30828609+subhashkvs@users.noreply.github.com>
2018-02-01 16:35:57 +0530

    Create kkk10.c

---
+++ kkk10.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a;
+printf("enter the number:");
+scanf("%d",&a);
+if(a%2==0)
+{
+    printf("\nyes");
+}
+else
+{
+    printf("\n no");
+}
+getch();
+}
