72df1f58d380855c8adb5e43e5ad58f35d379d53
subhashkvs <30828609+subhashkvs@users.noreply.github.com>
2018-02-02 09:27:46 +0000

    Create kvs57.c

---
+++ kvs57.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a,b,temp;
+    printf("enter the two number:");
+    scanf("%d %d",&a,&b);
+    temp=a;
+    a=b;
+    b=temp;
+    printf("%d %d",a,b);
+    getch();
+}
+    
+   
