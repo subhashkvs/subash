bc477ce3f98bcca96c5b3a4cbd512ece2503c23f
subhashkvs <30828609+subhashkvs@users.noreply.github.com>
2018-02-08 09:45:28 +0000

    Create 62.c

---
+++ 62.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+void main()
+{
+    char a[10];
+    int n,i,count;
+    printf("enter the number:");
+    scanf("%s",&a[i]);
+    n=strlen(a);
+    for(i=0;i<=n;i++)
+    {
+        if((a[i]==0) || (a[i]==1))
+        {
+          count++;
+       }
+    }
+
+    if(count=n)
+     {
+           printf("\nyes");
+        }
+       else
+       {
+           printf("no");
+       }
+    
+    
+    getch();
+}
+
