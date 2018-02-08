15f1b7d04390f8eda04e5ae8374a94519060290b
subhashkvs <30828609+subhashkvs@users.noreply.github.com>
2018-02-08 20:26:09 +0530

    Update 63.c

--- 63.c
+++ 63.c
@@ -1,16 +1,17 @@
+
 #include<stdio.h>
 #include<conio.h>
 #include<string.h>
 void main()
 {
     char a[10];
-    int n,i,count;
+    int n,i,count=0;
     printf("enter the number:");
     scanf("%s",&a[i]);
     n=strlen(a);
     for(i=0;i<n;i++)
     {
-        if(a[i]!="\o")
+        if(a[i]!=" ")
         {
             count++;
            
