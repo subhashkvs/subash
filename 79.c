297dbc2117af831195c93596e31675f947f43a4e
subhashkvs <30828609+subhashkvs@users.noreply.github.com>
2018-02-22 16:59:48 +0530

    Create 79.c

---
+++ 79.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,n,i;
+printf("enter the number:");
+scanf("%d%d",&a,&b);
+if(a==b)
+{
+    printf("it is correct");
+}
+else
+{
+    printf("it is wrong");
+}
+getch();
+}
