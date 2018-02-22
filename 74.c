ef93d61799822781c98870d331bf39b75d84575f
subhashkvs <30828609+subhashkvs@users.noreply.github.com>
2018-02-22 16:24:52 +0530

    Create 74.c

---
+++ 74.c
@@ -0,0 +1,11 @@
+#include <stdio.h> 
+#include<conio.h>
+#include<math.h>
+void main() 
+{
+float n;
+printf("Enter the number:");
+scanf("%f",&n);
+printf("round of number is %f",round(n));
+getch();
+}
