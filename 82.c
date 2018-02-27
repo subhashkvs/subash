27742ab6d8efa5159853551a920c1fa0bd5ca13d
subhashkvs <30828609+subhashkvs@users.noreply.github.com>
2018-02-27 16:22:52 +0530

    Create 82.c

---
+++ 82.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+float l,b,n;
+printf("enter the length:");
+scanf("%f",&l);
+printf("enter the breath:");
+scanf("%f",&b);
+n=l*b;
+printf("area is %f",n);
+getch();
+}
