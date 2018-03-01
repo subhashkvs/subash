964136e8c47169a374797fe997c1ff8cbe31e0f7
subhashkvs <30828609+subhashkvs@users.noreply.github.com>
2018-03-01 16:35:17 +0530

    Create 87.c

---
+++ 87.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(void) 
+{
+int n1,n2,gcd,lcm,i;
+printf("enter the two number:");
+scanf("%d%d",&n1,&n2);
+for(i=1;i<=n1 && i<=n2;i++)
+{
+if(n1%i==0 && n2%i==0)
+{
+gcd=i;
+}
+}
+printf("%d",gcd);
+getch();
+}
