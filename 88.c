26cc1c6ff64ee6de1a2caaa57373faf1d3d287ff
subhashkvs <30828609+subhashkvs@users.noreply.github.com>
2018-03-01 16:27:18 +0530

    Create 88.c

---
+++ 88.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(void) 
+{
+int n1,n2,gcd,lcm,i;
+printf("enter the two number:");
+scanf("%d%d",&n1,&n2);
+for(i=1;i<=n1 && i<=n2;i++)
+{
+if(n1%i==0 && n2%i==0)
+{
+gcd=i;
+}
+}
+lcm=(n1*n2)/gcd;
+printf("%d",lcm);
+getch();
+}
