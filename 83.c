71e435bb6e85e866a5ac399ed2ade221a9a10f86
subhashkvs <30828609+subhashkvs@users.noreply.github.com>
2018-03-01 16:56:46 +0530

    Update 83.c

--- 83.c
+++ 83.c
@@ -1,21 +1,22 @@
 #include<stdio.h>
+#include<conio.h>
 void main()
 {
-	int a,b,i,n;
-	char c;
-	printf("Enter the Number of Operations:");
-	scanf("%d",&n);
-	for(i=0;i<n;i++)
-	{
-		scanf("%d%c%d",&a,&c,&b);
-		if(i%2==0)
-		{
-			printf("\n%d",(a/b));
-		}
-		else
-		{
-			printf("\n%d",(a%b));
-		}
-	}
-	
+int a,b,i,n;
+char c;
+printf("Enter the Number:");
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+scanf("%d%c%d",&a,&c,&b);
+if(i%2==0)
+{
+	printf("\n%d",(a/b));
+}
+else
+{
+printf("\n%d",(a%b));
+}
+}
+getch();	
 }
