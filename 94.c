f1397a556a157efcc617f932018c81608c1317c5
subhashkvs <30828609+subhashkvs@users.noreply.github.com>
2018-03-02 16:07:14 +0530

    Create 94.c

---
+++ 94.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+void main()
+{
+    int a[10]={3,1,2,5,6,7,9,8},n;
+    printf("enter the number:");
+    scanf("%d",&n);
+    printf("%d",a[n-1]);
+    getch();
+}
+
