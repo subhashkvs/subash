2ac5cde4b10c1fe20a0608a73bdc47e1b93237ec
subhashkvs <30828609+subhashkvs@users.noreply.github.com>
2018-03-02 16:23:48 +0530

    Create 99.c

---
+++ 99.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int n,i,a,b,c;
+    printf("enter the three number:");
+    scanf("%d%d%d",&a,&b,&c);
+    n=a*b%c;
+ printf("%d",n);   
+}
