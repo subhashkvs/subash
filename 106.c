9abe24907287a2d5be9bbfdeaf6100344b13e3db
subhashkvs <30828609+subhashkvs@users.noreply.github.com>
2018-03-08 16:52:25 +0530

    Create 106.c

---
+++ 106.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+void main()
+{
+        int n;
+        printf("enter the number:");
+        scanf("%d",&n);
+        printf("the chracter is %c",n+64);
+        getch();
+}
