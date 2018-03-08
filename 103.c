862fac87b7e61b6d050a4cce8b619de3e921d014
subhashkvs <30828609+subhashkvs@users.noreply.github.com>
2018-03-08 16:28:03 +0530

    Create 103.c

---
+++ 103.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+void main()
+{
+        char a[10],n;
+        printf("enter the number");
+        scanf("%s",a);
+        printf("the upper case is %s",strupr(a));
+        getch();
+}
+
