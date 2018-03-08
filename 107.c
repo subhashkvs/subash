3eebb958f882ad09a3780c84d6eb809b5962bbaf
subhashkvs <30828609+subhashkvs@users.noreply.github.com>
2018-03-08 16:58:24 +0530

    Create 107.c

---
+++ 107.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+void main()
+{
+        int a,b,n,c;
+        printf("enter the two number:");
+        scanf("%d%d%d",&a,&b,&c);
+        n=(a*b)/c;
+        printf("\n %d",n);
+        getch();
+}
+
