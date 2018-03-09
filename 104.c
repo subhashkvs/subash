92afbac962bfd19ab5dbe58ddac150e118ca3788
subhashkvs <30828609+subhashkvs@users.noreply.github.com>
2018-03-09 16:24:30 +0530

    Create 104.c

---
+++ 104.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a,b,i,n=1;
+    printf("enter the number");
+    scanf("%d%d",&a,&b);
+    n=pow(a,b);
+    printf("\n%d",n);
+    getch();
+}
