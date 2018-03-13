649b9346471337bf3c936448758074a9154ff00e
subhashkvs <30828609+subhashkvs@users.noreply.github.com>
2018-03-13 16:02:56 +0530

    Create 112.c

---
+++ 112.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+void main()
+{
+    int n,a[100],b,i;
+    printf("enter the limit:");
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    printf("enter the number");
+    scanf("%d",&b);
+    for(i=0;i<n;i++)
+    {
+        if(a[i]==b)
+        {
+            printf("yes");
+        }
+        else
+        {
+            printf("no");
+        }
+    }
+    getch();
+}
